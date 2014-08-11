a37f1ad869c0d8574aaa669f7aa3e49c95103a1a
Kieren Evans <kevans@inviqa.com>
2014-08-11 10:17:24 +0100

    TMUP-271: Add database image.
    Based upon the base image, provision the image via
    chef-solo to install MySQL server and client and configure to
    allow networking from anywhere.
    
    We are using volumes, therefore our /var/lib/mysql gets wiped out
    upon the first invocation of the container after being built.
    
    To get around this, we back up the generated upon install
    /var/lib/mysql/ to /var/lib/mysql-container/.
    We have a custom MySQL Startup script, start-mysql.sh, to
    copy over the existing base data to the empty volume.
    
    Usage:
        $ vagrant up db --provider=docker

---
+++ tools/docker/db/chef/site-cookbooks/mysql-container/metadata.rb
@@ -0,0 +1,9 @@
+name             'mysql-container'
+maintainer       'Kieren Evans'
+maintainer_email 'kevans@inviqa.com'
+license          'Apache 2.0'
+description      'MySQL - in a container!'
+long_description 'MySQL needs a socket file outside of the context of our container, let's make a directory for it!'
+version          '0.1.0'
+
+depends 'mysql'
