95642c1349a814940586dbfc7db504eeba9f236b
Kieren Evans <kevans@inviqa.com>
2014-08-11 10:53:02 +0100

    TMUP-271: Cleanup
    Replace hardcoded values with variables that hobo would replace.
    Remove unneeded cookbooks.
    Delete commented out requirements from Berksfile.

--- tools/chef/site-cookbooks/docker-permissions/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'docker-permissions'
-maintainer       'Kieren Evans'
-maintainer_email 'kevans@inviqa.com'
-license          'Apache 2.0'
-description      'Installs/Configures chef-docker'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
--- tools/chef/site-cookbooks/knife-container/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'knife-container'
-maintainer       'Kieren Evans'
-maintainer_email 'kevans@inviqa.com'
-license          'Apache 2.0'
-description      'Installs/Configures knife-container'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
